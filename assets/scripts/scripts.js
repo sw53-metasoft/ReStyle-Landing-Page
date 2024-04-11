268061b240ad633f163e80e995e2c55d9c308ea6
Josten Huaman <jostenhuamanbellido@gmail.com>
2024-04-11 14:18:35 -0500

    feat(header): added styles

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -4,8 +4,8 @@ check.addEventListener('click',idioma);
 function idioma(){
     let id = check.checked;
     if( id == true){
-        location.href = "/ReStyle-landing-page/index.html"
+        location.href = "/ReStyle-Landin-Page/index.html"
     }else{
-        location.href = "/ReStyle-landing-page/index-en.html"
+        location.href = "/ReStyle-Landin-Page/index-en.html"
     }
 }
